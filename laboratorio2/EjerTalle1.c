1c52ed334f1693bdb58a5d7204beef1af4cef70d
GREGOROVITCH <117107692+GregorioNavarrete@users.noreply.github.com>
2023-09-28 10:20:20 -0300

    Update EjerTalle1.c
    Ejercicio listo para entregar

--- laboratorio2/EjerTalle1.c
+++ laboratorio2/EjerTalle1.c
@@ -6,7 +6,7 @@ struct Empleado {
     char nombre[50];
 };
 
-void leerEmpleado(struct Empleado *empleado);
+void leerEmpleado(int *legajo, char *nombre);
 int controlarLegajo(int legajo, int valorIngresado);
 void mostrarEmpleado(struct Empleado emp);
 
@@ -15,21 +15,30 @@ int main() {
     int opcion, valorIngresado;
 
     do {
+    	//muetras el menu 
         printf("Taller 1: Empleados\n");
         printf("1- Ingresar empleados\n");
         printf("2- Mostrar todos\n");
-        printf("3- Mostrar los legajos múltiplos\n");
-        printf("4- Mostrar los legajos no múltiplos\n");
+        printf("3- Mostrar los legajos multiplos\n");
+        printf("4- Mostrar los legajos no multiplos\n");
         printf("0- Salir\n");
-        printf("Ingrese su opción: ");
+        printf("Ingrese su opcion: ");
         scanf("%d", &opcion);
-
+        printf("\n");
+		printf("\n");
+		printf("\n");
+		
         switch (opcion) {
             case 1:
                 for (int i = 0; i < 2; i++) {
-                    printf("\nIngrese información del empleado %d:\n", i + 1);
-                    leerEmpleado(&empleados[i]);
+                    printf("\nIngrese informacion del empleado %d:\n", i + 1);
+                   
+                    leerEmpleado(&empleados[i].legajo,empleados[i].nombre);
+
                 }
+                printf("\n");
+                printf("\n");
+                printf("\n");
                 break;
             case 2:
                 printf("\nListado de todos los empleados:\n");
@@ -37,34 +46,51 @@ int main() {
                 for (int i = 0; i < 2; i++) {
                     mostrarEmpleado(empleados[i]);
                 }
+                printf("\n");
+                printf("\n");
+                printf("\n");
                 break;
             case 3:
-                printf("\nIngrese un número para verificar legajos múltiplos: ");
+                printf("\nIngrese un numero para verificar legajos multiplos: ");
                 scanf("%d", &valorIngresado);
-                printf("\nListado de empleados con legajos múltiplos de %d:\n", valorIngresado);
+                printf("\nListado de empleados con legajos multiplos de %d:\n", valorIngresado);
                 printf("LEGAJO\tNOMBRE\n");
                 for (int i = 0; i < 2; i++) {
                     if (controlarLegajo(empleados[i].legajo, valorIngresado) == 1 ) {
                         mostrarEmpleado(empleados[i]);
                     }
                 }
+                
+                printf("\n");
+                printf("\n");
+                printf("\n");
                 break;
             case 4:
-                printf("\nIngrese un número para verificar legajos no múltiplos: ");
+                printf("\nIngrese un numero para verificar legajos no multiplos: ");
                 scanf("%d", &valorIngresado);
-                printf("\nListado de empleados con legajos no múltiplos de %d:\n", valorIngresado);
+                printf("\nListado de empleados con legajos no multiplos de %d:\n", valorIngresado);
                 printf("LEGAJO\tNOMBRE\n");
                 for (int i = 0; i < 2; i++) {
                     if (controlarLegajo(empleados[i].legajo, valorIngresado) ==0) {
                         mostrarEmpleado(empleados[i]);
                     }
                 }
+                
+                printf("\n");
+                printf("\n");
+                printf("\n");
                 break;
             case 0:
                 printf("\nSaliendo del programa.\n");
+                printf("\n");
+                printf("\n");
+                printf("\n");
                 break;
             default:
-                printf("\nOpción no válida. Por favor, ingrese una opción válida.\n");
+                printf("\nOpcion no valida. Por favor, ingrese una opción valida.\n");
+                 printf("\n");
+                printf("\n");
+                printf("\n");
                 break;
         }
 
@@ -73,23 +99,38 @@ int main() {
     return 0;
 }
 
-void leerEmpleado(struct Empleado *empleado) {
-	/*
-	a) void leerEmpleado(int *legajo, char *nombre);
-Objetivo: Solicita los datos relativos a un empleado y verifica que sean válidos.
 
-no hace lo que me pide 
-	*/
-  
-    printf("Ingrese el legajo: ");
-    scanf("%d", &empleado->legajo); 
-    // la flecha es para asignar el valor a "el campo legajo de empleado"
-  
-    
 
-    printf("Ingrese el nombre del empleado: ");
-    scanf("%s", empleado->nombre);//por ser char no usa & ???
+
+void leerEmpleado(int *legajo, char *nombre) {
+    /*
+    Descripción: Solicita los datos relativos a un empleado y verifica que sean válidos.
+
+    Variables de entrada y salida:
+    - legajo: Puntero a entero donde se almacenará el legajo válido.
+    - nombre: Puntero a un arreglo de caracteres donde se almacenará el nombre del empleado.
+    */
+
+    int aux = 0;
+
+    do {
+        printf("Ingrese el legajo entre 1000 y 9999: ");
+        scanf("%d", &aux);
+
+        // Leer el primer caracter del nombre para verificar si es una letra, y no ingresar solo numeros o simbolos 
+        printf("Ingrese el nombre del empleado: ");
+        scanf("%s", nombre);
+        
+        // Verificar si el legajo y el primer carácter del nombre cumplen con los criterios.
+        if (aux >= 1000 && aux <= 9999 && ((nombre[0] >= 'A' && nombre[0] <= 'Z') || (nombre[0] >= 'a' && nombre[0] <= 'z'))) {
+            *legajo = aux; // Asigna el valor al campo, con puntero.
+        } else {
+            printf("\n----------->!!! Legajo fuera del rango o nombre no valido. Ingrese valores correctos.\n");
+        }
+
+    } while ((aux < 1000 || aux > 9999) || !((nombre[0] >= 'A' && nombre[0] <= 'Z') || (nombre[0] >= 'a' && nombre[0] <= 'z')));
 }
+
 int controlarLegajo(int legajo, int valorIngresado){
     //lo hago una funcion para poder usarlo 2 veces 
   
@@ -101,11 +142,16 @@ int controlarLegajo(int legajo, int valorIngresado){
     return aux;
 }
 
+
 void mostrarEmpleado(struct Empleado emp){
+	/*
+	Descripcion:: muestra los datos de un solo empleado. Se utiliza para generar el listado encolumnado.
+	Variables de entra y salida : un registro de entrada 
+
+	*/
   // tengo que decir que el dato es de tipo registro empleado
     printf("%d\t%s\n", emp.legajo, emp.nombre);
 }
-
 
 
 
